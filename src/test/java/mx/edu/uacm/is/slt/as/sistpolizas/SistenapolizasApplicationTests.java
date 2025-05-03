d2ca6ba0be05fcce4e08e68efdbddb4b8f612a38
Enrique <enrique.delarosa.ramos@estudiante.uacm>
2025-05-03 03:52:08 -0600

    Iniciando proyecto

---
+++ src/test/java/mx/edu/uacm/is/slt/as/sistpolizas/SistenapolizasApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.uacm.is.slt.as.sistpolizas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistenapolizasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
