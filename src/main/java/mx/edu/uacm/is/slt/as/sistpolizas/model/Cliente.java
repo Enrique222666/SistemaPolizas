c0c4f6b0f43fbd80a22f7af44421fc8c2077e6df
Enrique <enrique.delarosa.ramos@estudiante.uacm>
2025-05-03 07:28:20 -0600

    se creo y agrego la clase cliente

---
+++ src/main/java/mx/edu/uacm/is/slt/as/sistpolizas/model/Cliente.java
@@ -0,0 +1,73 @@
+package mx.edu.uacm.is.slt.as.sistpolizas.model;
+
+
+public class Cliente {
+
+    private String nombres;
+    private String primerApellido;
+    private String segundoApellido;
+    private String direccion;
+    private String curp;
+    private String fechaNacimiento;
+
+    // Constructor
+    public Cliente(String nombres, String primerApellido, String segundoApellido,
+                   String direccion, String curp, String fechaNacimiento) {
+        this.nombres = nombres;
+        this.primerApellido = primerApellido;
+        this.segundoApellido = segundoApellido;
+        this.direccion = direccion;
+        this.curp = curp;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    // Getters y Setters
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+
+    public String getSegundoApellido() {
+        return segundoApellido;
+    }
+
+    public void setSegundoApellido(String segundoApellido) {
+        this.segundoApellido = segundoApellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCurp() {
+        return curp;
+    }
+
+    public void setCurp(String curp) {
+        this.curp = curp;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+    
+}
