28c028d759f11a05a7501127a35c402d902a0fae
Enrique <enrique.delarosa.ramos@estudiante.uacm>
2025-06-11 18:16:27 -0600

    se corrige error de ejecucion

--- src/main/java/mx/edu/uacm/is/slt/as/sistpolizas/AuxiliarF/Convertir.java
+++ src/main/java/mx/edu/uacm/is/slt/as/sistpolizas/AuxiliarF/Convertir.java
@@ -1,28 +1,25 @@
 package mx.edu.uacm.is.slt.as.sistpolizas.AuxiliarF;
 
 import java.time.LocalDate;
+import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.UUID;
 
 public class Convertir {
 
-    /**
-     * Convierte un String a LocalDate usando el formato ISO (yyyy-MM-dd).
-     * @param fechaStr la cadena con la fecha
-     * @return LocalDate o null si hay error
-     */
-    public static LocalDate stringALocalDate(String fechaStr) {
+    public static OffsetDateTime stringAOffsetDateTime(String fechaStr) {
         try {
-            return LocalDate.parse(fechaStr, DateTimeFormatter.ISO_LOCAL_DATE);
+            return OffsetDateTime.parse(fechaStr, DateTimeFormatter.ISO_OFFSET_DATE_TIME);
         } catch (DateTimeParseException e) {
-            System.err.println("Formato de fecha inválido: " + fechaStr);
+            System.err.println(" Formato de fecha y hora inválido: " + fechaStr);
             return null;
         }
     }
 
     /**
      * Convierte un String a int.
+     *
      * @param numeroStr la cadena con el número
      * @return int o 0 si hay error
      */
@@ -37,6 +34,7 @@ public class Convertir {
 
     /**
      * Convierte un String a float.
+     *
      * @param numeroStr la cadena con el número
      * @return float o 0.0f si hay error
      */
@@ -49,5 +47,26 @@ public class Convertir {
         }
     }
 
-
+    /**
+     * Convierte un String a UUID.
+     *
+     * @param uuidStr la cadena con el UUID
+     * @return UUID o null si hay error
+     */
+    public static UUID stringAUUID(String uuidStr) {
+        try {
+            return UUID.fromString(uuidStr);
+        } catch (IllegalArgumentException e) {
+            System.err.println("UUID inválido: " + uuidStr);
+            return null;
+        }
+    }
+        //reemplazarEspaciosPorGuion
+        public static String espaciosAGuion(String entrada) {
+        if (entrada == null) {
+            return null;
+        }
+        return entrada.contains(" ") ? entrada.replace(" ", "-") : entrada;
+    }
+    
 }
